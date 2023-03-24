6a4e76950e28fdcb4c05945462f5ef4dc0586d2f
Farashjr <98732688+Farashjr@users.noreply.github.com>
2023-03-24 06:40:19 +0600

    can you correct it

---
+++ app/src/main/java/com/example/v_registration_login/ProductSearch.java
@@ -0,0 +1,105 @@
+package com.example.v_registration_login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class ProductSearchActivity extends AppCompatActivity {
+    private EditText searchEditText;
+    private RecyclerView searchRecyclerView;
+    private FirebaseFirestore db;
+    private FirestoreRecyclerAdapter<Product, ProductViewHolder> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_search);
+
+        searchEditText = findViewById(R.id.search_edit_text);
+        searchRecyclerView = findViewById(R.id.search_recycler_view);
+        searchRecyclerView.setHasFixedSize(true);
+        searchRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                firestoreProductSearch(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void firestoreProductSearch(String searchText) {
+        Query query = db.collection("Products")
+                .orderBy("productName")
+                .startAt(searchText)
+                .endAt(searchText + "\uf8ff");
+
+        FirestoreRecyclerOptions<Product> options = new FirestoreRecyclerOptions.Builder<Product>()
+                .setQuery(query, Product.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<Product, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Product model) {
+                holder.setProductDetails(getApplicationContext(), model.getProductName(), model.getProductImage(), model.getProductPrice());
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.product_item, parent, false);
+                return new ProductViewHolder(view);
+            }
+        };
+
+        adapter.startListening();
+        searchRecyclerView.setAdapter(adapter);
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setProductDetails(Context ctx, String productName, String productImage, String productPrice) {
+            TextView name = mView.findViewById(R.id.product_name);
+            ImageView image = mView.findViewById(R.id.product_image);
+            TextView price = mView.findViewById(R.id.product_price);
+
+            name.setText(productName);
+            Picasso.get().load(productImage).into(image);
+            price.setText(productPrice);
+        }
+    }
+}
