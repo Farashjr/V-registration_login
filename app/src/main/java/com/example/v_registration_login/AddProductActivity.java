4c0bd59a098e64911a2855b93d864d77ea9d48e1
Farashjr <98732688+Farashjr@users.noreply.github.com>
2023-04-06 17:59:03 +0600

    fix conflicts

--- app/src/main/java/com/example/v_registration_login/AddProductActivity.java
+++ app/src/main/java/com/example/v_registration_login/AddProductActivity.java
@@ -1,8 +1,5 @@
 package com.example.v_registration_login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
