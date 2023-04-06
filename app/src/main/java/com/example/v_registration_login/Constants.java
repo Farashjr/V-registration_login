018b10a2b6c5444226475830b7a252146366fff0
Farashjr <98732688+Farashjr@users.noreply.github.com>
2023-04-06 17:22:23 +0600

    -change: AddProductActivity but still some errors -add: MainSellerActivity.java with the xml part check for errors and correct

---
+++ app/src/main/java/com/example/v_registration_login/Constants.java
@@ -0,0 +1,18 @@
+package com.example.v_registration_login;
+
+public class Constants {
+
+    public static final String[] productCategories= {
+            "Fruits",
+            "Vegetables",
+            "Dairy products",
+            "Meat and poultry",
+            "Seafood",
+            "Canned and jarred foods",
+            "Grains and cereals",
+            "Snacks and confectionery",
+            "Beverages",
+            "Baking and cooking supplies",
+            "others"
+    };
+}
